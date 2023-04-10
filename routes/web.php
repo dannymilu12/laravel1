81368c0a4564a92f4f927bfb44cb872ab72a3e91
Daniel C <dannymilu12@gmail.com>
2023-04-10 16:35:59 -0500

    Video 92_CRUD + RUTAS TIPO RECURSO AGRUPADAS

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,51 @@ Route::get('/',function(){
     return view('welcome'); 
 });
 
+// parametros en rutas (? = Opcional)
+
+// Route::get('/test/{id?}/{name?}',function($id = 10, $name='DC'){  // ? = opcional
+//     echo $id;    
+//     echo $name;
+// });
+
+// Rutas Agrupadas Consultar no funciono en video
+
+// Route::controller(PostController::class)->group(function(){
+//     Route::get('post', 'index')->name("post.index");
+//     Route::get('post/{post}', 'show')->name("post.show");
+//     Route::get('post/create', 'create')->name("post.create");
+//     Route::get('post/{post}/edit', 'edit')->name("post.edit");
+//     Route::post('post', 'store')->name("post.store");
+//     Route::put('post/{post}', 'update')->name("post.update");
+//     Route::delete('post/{post}', 'delete')->name("post.destroy");
+// });
+
+ // Agrupacion de middleware
+//  Route::middleware([App\Http\Middleware\TestMiddleware::class])->group(function(){
+//     Route::get('/test/{id?}/{name?}',function($id=10,$name= 'DC'){
+//             echo $id;
+//             echo $name;
+//         });
+// });
+
+// agrupacion grupo de rutas, mas utilizada.
+// route::group(['prefix' => 'dashboard'],function(){
+//     Route::resource('post', postController::class);
+//     Route::resource('category', CategoryController::class);
+// });
+
+// agrupacion de rutas tipo recurso
+
+route::group(['prefix' => 'dashboard'],function(){    
+    Route::resources([
+        'post' => postController::class,
+        'category' => CategoryController::class,
+    ]);  
+});  
+
+
 // ruta tipo recurso
-Route::resource('post', postController::class);
+//Route::resource('post', postController::class);
 
 // Metodo  de controlador post controller, si no se utiliza la ruta tipo recurso
 
@@ -34,7 +77,7 @@ Route::resource('post', postController::class);
 // Route::delete('post/{post}', [postController::class,'delete']);
 
 // ruta para administracion de las categorias
-Route::resource('category', CategoryController::class);
+//Route::resource('category', CategoryController::class);
 
 
 
